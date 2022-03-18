196f385dc0cca3ef0658635662a6e635ee72c865
Archana <archanasi405@gmail.com>
2022-03-18 13:59:13 +0530

    Initial commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  longText: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onClick(){
+    console.log('dashboard works');
+    this.longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+  }
+
+}
